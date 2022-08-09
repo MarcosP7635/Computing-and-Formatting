378c75b549f35a58fb1b67c24324d73657292e29
MarcosP7635 <marcos.aaron.perez@gmail.com>
2022-08-08 18:03:54 -0700

    works and rewrote it to make iteration easier

--- automate_DPASS.py
+++ automate_DPASS.py
@@ -1,13 +1,12 @@
 from multiprocessing import Process, freeze_support, Pool
 from automate_local_typing import *
 if __name__ == '__main__':
-    wait_time = 60
     output_dir = "C:\\Users\\engin\\Documents\\GitHub\\Energy\\ImportedData\\DPASS_Output"
     images_dir = "C:\\Users\engin\\Downloads\\DPASS_icons"
     program_path = ["C:\\Users\\engin\\Downloads\\DPASS2106\\DPASS2106\\DPASS.exe"]
     pool = Pool(processes=2)              # start 4 worker processes
     result = pool.apply_async(os.system, program_path)
-    result2 = pool.apply_async(enter_inputs, (images_dir, output_dir))
-    result2.get(timeout= wait_time)    #run the function for at most a given time in seconds
+    result2 = pool.apply_async(find_image_centers, (images_dir, output_dir))
+    result2.get()    #run the function asynchronously
     result.get()                  
 
--- automate_local_typing.py
+++ automate_local_typing.py
@@ -1,4 +1,5 @@
 from asyncio import subprocess
+from email.mime import image
 import multiprocessing
 from pyautogui import *
 from subprocess import Popen, check_call
@@ -17,71 +18,48 @@ def edit_field(search_term):
 	keyUp('ctrl')
 	write(search_term)
 
-def enter_inputs(images_dir, output_dir, target = 1, projectile = 1):
-    edit_field(str(projectile))
-    click_button(images_dir + "\\target_image.PNG",
-                    direction = "Right", distance = 150)
-    edit_field(str(target))
-    time.sleep(.5)
-    click_button(images_dir + "\\raw_output_image.PNG", 
-                    direction = "Right", distance = 225)
-    time.sleep(.5)
-    keyDown('ctrl')
-    press('s')
-    keyUp('ctrl')
-    time.sleep(.5)
-    write(output_dir + "\\" + str(target) + " " + str(projectile))
-    press('Enter')
-    return 0
-
-def run_local_process(wait_time, function, function_args):
-	#run function for at most a given time in seconds
-    multiprocessing.freeze_support()
-    p = multiprocessing.Process(target = function, name= "function",
-        args = function_args)
-    p.start()
-    print("started the loop")
-    # Wait 10 seconds for foo
-    time.sleep(wait_time)
-    # Terminate foo
-    p.terminate()
-    # Cleanup
-    p.join()
-    print("Ran", function, "(", function_args, ")", "for ", wait_time,
-        "seconds")
-
 def click_on_pixel(pixel):
 	x, y = pixel[0], pixel[1]
 	click(x, y)
 
-def click_button(image_file_path, direction = "Left", distance = 100):
-	#Look for the image on the screen
-	while True:
-		image_center = locateCenterOnScreen(image_file_path)
-		print("looking for ", image_file_path)
-		if (image_center is not None):
-			if(direction == "Left"):
-				search_bar_location = (image_center[0] - distance, image_center[1])
-			elif(direction == "Right"):
-				search_bar_location = (image_center[0] + distance, image_center[1])
-			elif(direction == "Up"):
-				search_bar_location = (image_center[0], image_center[1] - distance)
-			elif(direction == "Down"):
-				search_bar_location = (image_center[0], image_center[1] + distance)
-			print("Search bar located at coordinates: ", search_bar_location)
-			break
-	click_on_pixel(search_bar_location)
-	return search_bar_location
+def click_button_known_location(image_center, direction = "Left", distance = 100):
+    if (image_center is not None):
+        if(direction == "Left"):
+            search_bar_location = (image_center[0] - distance, image_center[1])
+        elif(direction == "Right"):
+            search_bar_location = (image_center[0] + distance, image_center[1])
+        elif(direction == "Up"):
+            search_bar_location = (image_center[0], image_center[1] - distance)
+        elif(direction == "Down"):
+            search_bar_location = (image_center[0], image_center[1] + distance)
+        click_on_pixel(search_bar_location)
+        return 0
+    return 1
 
-def collect_data( 
-		output_dir = "C:\\Users\\engin\\Documents\\GitHub\\Energy\\ImportedData\\DPASS_Output",
-		images_dir = "C:\\Users\engin\\Downloads\\DPASS_icons",
-		wait_time = 15):
-    run_local_process(wait_time = wait_time, function = enter_inputs, 
-                    function_args= (images_dir, output_dir))
-
-def find_image_centers(images_dir):
+def find_image_centers(images_dir, output_dir):
     image_centers = {}
-    for image_file in os.listdir(images_dir):
-        image_centers[image_file] = locateCenterOnScreen(images_dir + "\\" + image_file)
+    while True:
+        for image_file in os.listdir(images_dir):
+            image_centers[image_file] = locateCenterOnScreen(images_dir + "\\" + image_file)
+        if None not in image_centers.values():
+            break
+    enter_inputs(output_dir, image_centers)
     return image_centers
+
+def enter_inputs(output_dir, image_centers, target = 1, projectile = 1):
+    click_button_known_location(image_centers["projectile_image.PNG"],
+                    direction = "Right", distance = 150)
+    edit_field(str(projectile))
+    click_button_known_location(image_centers["target_image.PNG"],
+                    direction = "Right", distance = 150)
+    edit_field(str(target))
+    click_button_known_location(image_centers["raw_output_image.PNG"],
+                    direction = "Right", distance = 225)
+    time.sleep(.1)
+    keyDown('ctrl')
+    press('s')
+    keyUp('ctrl')
+    time.sleep(.1)
+    write(output_dir + "\\" + str(target) + " " + str(projectile) + " raw_output")
+    press('Enter')
+    return 0
